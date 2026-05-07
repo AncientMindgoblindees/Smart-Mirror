2d5a4ff321b66771de56f0cb064b632d56d13d80
tjmeltesen <tjmeltesen@gmail.com>
2026-05-07 12:21:23 -0500

    starter script

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { AppRouter } from '@/app/AppRouter';
 import '@/index.css';
 
+// Hide cursor immediately on load
+document.documentElement.style.cursor = "none";
+document.body.style.cursor = "none";
+
+// Then render your app
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AppRouter />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
